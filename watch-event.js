ca9a8343c496bb03b0477f41d30b5dc2a821730c
gulshanpr <gulshankumarprasad@proton.me>
2025-04-30 15:51:04 +0530

    final commit after demo

--- watch-event.js
+++ watch-event.js
@@ -18,7 +18,8 @@ const anvil = defineChain({
 });
 
 // Replace with your deployed contract's address
-const contractAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
+
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 // Create a Viem public client
 const publicClient = createPublicClient({
